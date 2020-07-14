90f5267729c60bb64632fc9d52d134dba1ddb749
CodingGengar <scriptkiddie231@protonmail.ch>
2020-07-14 04:15:16 -0700

    Added Prefix and Command

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Command Prefix
+const prefix = '!';
 
 
 // This will notify when the bot is up
 
 client.once('ready', () => {
-    console.log('Hello my name is GIDEON')
+    console.log('Hello my name is GIDEON');
 });
 
 
 
+// Command Line
+
+client.on('message' , message => {
+    if (msg.content === '!meep') {
+        msg.reply('MEEP MY BOIZ MMMMMMMEEEEEEEEEPPPPPP!!!');
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Never put token in code
-client.login('token goes here');
+client.login('token here');
